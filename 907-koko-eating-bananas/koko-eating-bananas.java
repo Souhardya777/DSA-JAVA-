bbd8c8df559475be9802a9d72d94bfc9a898a0fa
Souhardya Banerjee <souhardya1122@gmail.com>
2025-12-08 01:33:25 +0530

    Time: 6 ms (99.46%) | Memory: 47.7 MB (47.69%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,57 +1,27 @@
-//class Solution {
-//    public int minEatingSpeed(int[] piles, int h) {
-//        int high =0;
-//        for(int e : piles){
-//            if(e>high)
-//            high=e;
-//        }
-//        int low = 1;
-//        while(low<high){
-//            int mid = low+(high-low)/2;
-//            if(hour(piles,mid)<=h){
-//                high = mid;
-//            }
-//            else
-//                low = mid+1;
-//        }
-//        return low;
-//    }
-
-//    private long hour(int[] piles, int k){
-//        long total =0L;
-//        for(int p : piles){
-//            total += (p+k-1)/k;
-//        }
-//        return total;
-//    }
-//}
-
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-    int n = piles.length;
-        long total = 0;
-        int s =0;
-        for (int p : piles){
-            total += p;
-            if(p > s)
-                s=p;
+        int high =0;
+        for(int e : piles){
+            if(e>high)
+            high=e;
         }
-        
-        int left = (int) Math.ceil((double)(total/h));
-        int right = s;
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            int time = 0;
-            for (int p : piles) {
-                time += Math.ceil((double)p/ mid);
+        int low = 1;
+        while(low<high){
+            int mid = low+(high-low)/2;
+            if(hour(piles,mid)<=h){
+                high = mid;
             }
-            if (time > h) {
-                left = mid + 1;
-            } else {
-                right = mid;
-            }
+            else
+                low = mid+1;
         }
-        return left;
+        return low;
+    }
+
+    private long hour(int[] piles, int k){
+        long total =0L;
+        for(int p : piles){
+            total += (p+k-1)/k;
+        }
+        return total;
     }
 }
